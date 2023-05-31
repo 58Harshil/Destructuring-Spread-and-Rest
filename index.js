320ba9efe42ee37d422c56b521a95ff8b6c0ccdb
Harshil Choudhary <112849807+58Harshil@users.noreply.github.com>
2023-06-01 00:36:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,38 @@ const sitenmae = ["pwskills"]
 //console.log([...sitenmae]);
 //console.log(newnames);
 
+let empltySet = new Set()
+//console.log(empltySet.size);
 
+let myArray = [1,2,3,4,3,2]
+let newSet = new Set([...myArray])
+//console.log(newSet);
+
+newSet.add(9)
+//console.log(newSet);
+//console.log(newSet.has(9));
+newSet.clear()
+
+
+
+// set difference
+function setDiffrence(SetA, SetB){
+    return new Set([...SetA].filter(El => !SetB.has(el)))
+}
+
+
+let map = new Map()
+//console.log(map.size); 
+
+let arr = [
+    [1, "harshil"],
+    [2, "Karan"],
+    [3, "Riski"],
+    [4, "shivam"],
+    [5, "Loda"],
+];
+
+arr.map((arrayItem)=> map.Set(arrayItem[0], arrayItem[1]))
 function pwskills(...values){
     return values
 }
